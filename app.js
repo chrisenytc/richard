31eee8f268bb06714d0667b77e48b067687e66f6
Christopher EnyTC <chris@enytc.com>
2016-06-23 03:27:16 -0300

    Add server

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var request = require('superagent');
+var cheerio = require('cheerio');
+var app = express();
+// Configs
+var port = process.env.PORT || 3000;
+var url = 'http://restauranteflorida.com.br/pratododia.php';
+
+app.get('/', function (req, res) {
+	return request
+		.get(url)
+		.end(function(err, response){
+			if (err) {
+				return res.status(500).json(err);
+			}
+
+			var $ = cheerio.load(response.text);
+
+			var name = $('#prato_farialima').children().get(11).next.data;
+			var message = 'Essa é uma ótima hora para alimentar o Richard. O risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, '');
+
+			return res.status(200).json({
+				speech: message,
+				displayText: message,
+				source: 'richard-bot'
+			});
+		});
+});
+
+app.listen(port, function () {
+	console.log('Richard app listening on port ' + port + '!');
+});
