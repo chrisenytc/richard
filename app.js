5511618021a2d463ddb0f16fd2f650aaf2bb5dc0
Christopher EnyTC <chris@enytc.com>
2016-07-01 03:17:46 -0300

    Add random responses

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var request = require('superagent');
 var cheerio = require('cheerio');
+var _ = require('lodash');
 var app = express();
 // Configs
 var port = process.env.PORT || 3000;
@@ -20,7 +21,15 @@ app.all('/', function (req, res) {
 			var $ = cheerio.load(response.text);
 
 			var name = $('#prato_farialima').children().get(11).next.data;
-			var message = 'Essa é uma ótima hora para alimentar o Richard. O risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, '');
+			var messages = [
+				'Essa é uma ótima hora para alimentar o Richard. O risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, ''),
+				'Acho que você vai ficar muito feliz porque o risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, ''),
+				'Nem imagino com quanta fome você está, ainda bem que o risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, ''),
+				'Hoje é um dia especial porque o risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, ''),
+				'Não deixe o Richard esperando, corra logo para o Flórida. O risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, ''),
+				'Eu tenho uma surpresa para você. O risoto de hoje é ' + name.replace(/^\s+|\n|\r/g, '')
+			];
+			var message = messages[_.random(0, 5)];
 
 			return res.status(200).json({
 				speech: message,
